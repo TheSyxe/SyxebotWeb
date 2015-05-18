5ed763b9f78d5cb2cd7a98554bae2edf307e9691
TheSyxe <thesyxe@comcast.net>
2015-05-17 20:34:53 -0500

    Init

---
+++ SyxebotWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SyxebotWeb.Startup))]
+namespace SyxebotWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
